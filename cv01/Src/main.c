45cdca76e7b419e05a68722ccce787b8a4ca9626
mbebr97 <mikulas.bebr@gmail.com>
2022-09-21 12:13:47 +0200

    cv01 - LED blinking

--- cv01/Src/main.c
+++ cv01/Src/main.c
@@ -18,12 +18,29 @@
 
 #include <stdint.h>
 
+#include "stm32f0xx.h"
+
+
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+
+
 int main(void)
 {
+
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODER5_0;
+
+	for(;;) {
+	GPIOA->ODR ^= (1<<5); // toggle
+
+	for (volatile uint32_t i = 0; i < 100000; i++) {}
     /* Loop forever */
-	for(;;);
+
+
+
+
+	}
 }
